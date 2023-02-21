8f8e713353374f7596cc2f2a2cf28ed68c163654
Alexander Hopgood <alex.hopgood@gmail.com>
2023-02-21 10:33:20 +0000

    Adding exmaple of a factory method.

---
+++ src/main/java/com/alexander/designpatterns/factory/method/NYPizzaStore.java
@@ -0,0 +1,26 @@
+package com.alexander.designpatterns.factory.method;
+
+import com.alexander.designpatterns.factory.CheesePizza;
+import com.alexander.designpatterns.factory.ClamPizza;
+import com.alexander.designpatterns.factory.PepperoniPizza;
+import com.alexander.designpatterns.factory.Pizza;
+import com.alexander.designpatterns.factory.PizzaStore;
+import com.alexander.designpatterns.factory.VeggiePizza;
+
+public class NYPizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza();
+        }
+        return pizza;
+    }
+}
